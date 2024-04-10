73ffb4bada3826a795e53f949a744b0b8db93935
Arthur <arthurleg29@gmail.com>
2024-04-10 12:11:59 +0200

    Add files via upload

---
+++ Projet/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function displayAuthor(string $authorEmail, array $users): string
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+
+    return 'Auteur inconnu';
+}
+
+
+function redirectToUrl(string $url): never
+{
+    header("Location: {$url}");
+    exit();
+}
