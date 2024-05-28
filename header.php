b743232bb8a5753474e4825f4282f038fc79cab9
Arthur Le Gall <le-gall.arthur@ent-sfnd.fr>
2024-05-28 09:57:15 +0200

    Header is now selected when on page

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+
+$arr = explode('/', $uri);
+$string = implode('/',array_slice($arr, 3, 4));
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Site de listes</a>
@@ -7,23 +14,22 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link <?php echo ($string == 'index.php') ? 'active' : ''; ?>" aria-current="page" href="index.php">Home</a>
                 </li>
-                
                 <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="nouvelle_liste.php">Ajouter une liste</a>
+                        <a class="nav-link <?php echo ($string == 'nouvelle_liste.php') ? 'active' : ''; ?>" aria-current="page" href="nouvelle_liste.php">Ajouter une liste</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="shared.php">Mes partages</a>
+                        <a class="nav-link <?php echo ($string == 'shared.php') ? 'active' : ''; ?>" aria-current="page" href="shared.php">Mes partages</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Déconnexion</a>
+                        <a class="nav-link <?php echo ($string == 'logout.php') ? 'active' : ''; ?>" aria-current="page" href="logout.php">Déconnexion</a>
                     </li>
 
                 <?php else:?>
                     <li class="nav-item">
-                        <a class="nav-link" href="register.php">S'enregistrer</a>
+                        <a class="nav-link <?php echo ($string == 'register.php') ? 'active' : ''; ?>"aria-current="page" href="register.php">S'enregistrer</a>
                     </li>
                 <?php endif; ?>
             </ul>
